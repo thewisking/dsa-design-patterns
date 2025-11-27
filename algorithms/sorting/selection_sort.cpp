379d29bc25f1a122fe97085cd78cd6adcdcda35d
Thewisking <wissamdiab122@gmail.com>
2025-11-27 21:56:33 +1030

    improved selection sort. it works but wrongly

--- algorithms/sorting/selection_sort.cpp
+++ algorithms/sorting/selection_sort.cpp
@@ -4,22 +4,21 @@
 template <typename T>
 void SelectionSort<T>::sort(std::vector<T> &data) const {
 
-    auto size = data.size();
+    std::size_t size = data.size();
 
     if (size == 0) {
         return;
     }
-    auto currentMin = data[0];
-    auto currentItem = data[0];
-    
-    for(auto i = 0; i < size; i++){
-        for(auto j = i; j < size; j++){
-            if(currentItem > currentMin){
-                currentMin = currentItem;
+    int currentMin;
+
+    for (std::size_t i = 0; i < size; i++) {
+        currentMin = i;
+        for (std::size_t j = i; j < size; j++) {
+            if (data[j] > data[currentMin]) {
+                currentMin = j;
             }
         }
-        data[i] = currentMin;
-        currentMin = data[i+1];
+        data[i] = data[currentMin];
     }
     return;
 }
